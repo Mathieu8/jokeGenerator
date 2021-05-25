5dc338d6277026e7246a5839023a16f577ecd62d
Mathieu8 <mathieuvanommeren@gmail.com>
2021-05-25 11:30:09 +0200

    Initial commit

---
+++ src/test/java/com/example/jokegenerator/JokeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
